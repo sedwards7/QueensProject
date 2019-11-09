a4125ebb4eb5e20d7f2048d91831074bf3d395b5
sean <hxc.edwards@gmail.com>
2019-11-09 12:26:33 -0500

    Conflict Method working -Still need to ensure a and y values are less than the board dimensions

--- src/Queen.java
+++ src/Queen.java
@@ -1,16 +1,18 @@
 public class Queen
 {
-    private int row, column;
+    private int row, column, n;
 
     //sets Queen at 1,1 by default
     public Queen(){
         row = 1;
         column = 1;
+        n = 3;
     }
 
-    public Queen(int r, int c){
+    public Queen(int r, int c, int boardSize){
         row = r;
         column = c;
+        n = boardSize;
     }
 
     public int getRow(){
@@ -32,6 +34,46 @@ public class Queen
     public void setBoth(int r, int c){
         row = r;
         column = c;
+    }
+
+    public boolean conflict(Queen con){
+        int x = this.row;
+        int y = this.column;
+
+        if (x == con.getRow() || y == con.getRow() || checkDiagConflict(con))
+            return true;
+        else
+            return false;
+
+    }
+
+    public boolean checkDiagConflict(Queen con){
+        int x = this.row;
+        int y = this.column;
+        boolean answer = false;
+
+        // check left diagonal
+        while (x <= n && y <= n && x >= 1 && y >= 1){
+            if (con.getRow() == x && con.getColumn() == y)
+                answer = true;
+            //check diagonal space to the left
+            x--;
+            y--;
+        }
+        x = this.row;
+        y = this.column;
+        //check right diagonal
+        while (x <= n && y <= n && x >= 1 && y >= 1){
+            if (con.getRow() == this.getRow() && con.getColumn() == this.getColumn())
+                answer = true;
+            //check diagonal space to the right
+            x++;
+            y--;
+        }
+
+        return answer;
+
+
     }
 
     @Override
--- src/QueenSimulation.java
+++ src/QueenSimulation.java
@@ -1,3 +1,11 @@
 public class QueenSimulation
 {
+    public static void main (String[] args)
+    {
+        int n = 4;
+        Queen q1 = new Queen(1, 1, n);
+        Queen q2 = new Queen(1, 3, n);
+
+        System.out.println(q2.conflict(q1));
+    }
 }
